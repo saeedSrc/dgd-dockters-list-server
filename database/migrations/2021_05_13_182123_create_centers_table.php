5f2e54a1118996662d908bad625627870357a6f4
saeed <saeedrasooli421@gamil.com>
2021-05-14 01:38:30 +0430

    dgd

--- database/migrations/2021_05_13_182123_create_centers_table.php
+++ database/migrations/2021_05_13_182123_create_centers_table.php
@@ -18,26 +18,26 @@ class CreateCentersTable extends Migration
             $table->string('name');
             $table->string('site');
             $table->string('technical_manager_name');
-            $table->unsignedBigInteger('country');
-            $table->unsignedBigInteger('province');
-            $table->unsignedBigInteger('city');
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('province_id');
+            $table->unsignedBigInteger('city_id');
             $table->integer('area');
             $table->string('area_name');
             $table->integer('discount');
             $table->integer('satisfaction');
-            $table->unsignedBigInteger('hours_of_work');
+            $table->unsignedBigInteger('hours_of_work_id');
             $table->enum('governmental_type', ['governmental', 'private']);
-            $table->integer('type');
+            $table->unsignedBigInteger('type_id');
             $table->decimal('latitude');
             $table->decimal('longitude');
             $table->string('logo');
             $table->timestamps();
 
-            $table->foreign('country')->references('id')->on('countries')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('province')->references('id')->on('province')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('city')->references('id')->on('cities')->onUpdate('cascade')->onDelete('cascade');;
-            $table->foreign('hours_of_work')->references('id')->on('hours_of_works')->onUpdate('cascade')->onDelete('cascade');;
-            $table->foreign('type')->references('id')->on('center_types')->onUpdate('cascade')->onDelete('cascade');;
+            $table->foreign('country_id')->references('id')->on('countries')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('province_id')->references('id')->on('province')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('city_id')->references('id')->on('cities')->onUpdate('cascade')->onDelete('cascade');;
+            $table->foreign('hours_of_work_id')->references('id')->on('hours_of_works')->onUpdate('cascade')->onDelete('cascade');;
+            $table->foreign('type_id')->references('id')->on('center_types')->onUpdate('cascade')->onDelete('cascade');;
         });
     }
 
