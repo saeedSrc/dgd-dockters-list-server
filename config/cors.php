a971d422a03616418db9054edccec9ad8dcde65b
saeed <saeedrasooli421@gamil.com>
2021-08-06 13:45:59 +0430

    game round submit

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS'],
 
-//    'allowed_origins' => ['http://localhost:2223'],
-    'allowed_origins' => ['http://185.81.96.196'],
+    'allowed_origins' => ['http://localhost:2223'],
 
     'allowed_origins_patterns' => [],
 
