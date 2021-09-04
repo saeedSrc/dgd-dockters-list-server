b03baf4a1b794f248c597b8ef306a2002c7d2e79
saeed <saeedrasooli421@gamil.com>
2021-09-04 20:35:11 +0430

    logical event handler

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
 //            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => bcrypt("21$3ZZS!986d"), // password
+            'password' => bcrypt("09907576487"), // password
 //            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
